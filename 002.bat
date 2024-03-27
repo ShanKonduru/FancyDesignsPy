a4841d9713b64f6af9148c302803e7b3045c0879
Shan Konduru <shankonduru@gmail.com>
2024-03-27 20:02:40 +0530

    enhanced the batch file

--- 002.bat
+++ 002.bat
@@ -1,6 +1,38 @@
 @echo off
-REM poetry run python flower.py
-REM poetry run python flower2.py
-REM poetry run python concentric_circles.py
-REM poetry run python spirograph.py
+if '%1'=='1' goto ONE
+if '%1'=='2' goto TWO
+if '%1'=='3' goto THREE
+if '%1'=='4' goto FOUR
+if '%1'=='5' goto FIVE
+goto USAGE
+
+:ONE
+poetry run python flower.py
+goto END
+
+:TWO
+poetry run python flower2.py
+goto END
+
+:THREE 
+poetry run python concentric_circles.py
+goto END
+
+:FOUR
+poetry run python spirograph.py
+goto END
+
+:FIVE
 poetry run python spiralweb.py
+goto END
+
+:USAGE
+echo Usage:
+echo "002 1 => to Run Flower design"
+echo "002 2 => to Run flower2 design"
+echo "002 3 => to Run concentric_circles design"
+echo "002 4 => to Run spirograph design"
+echo "002 5 => to Run spiralweb design"
+goto END
+
+:END
