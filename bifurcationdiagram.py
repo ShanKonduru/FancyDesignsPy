3c9e58880da0fb35f167174ec296e135b6ffc052
Shan Konduru <shankonduru@gmail.com>
2024-03-28 15:25:43 +0530

    refactored the code

--- bifurcationdiagram.py
+++ bifurcationdiagram.py
@@ -1,23 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-Many =50000
+class LogisticMapPlotter:
+    def __init__(self, Many=50000):
+        self.Many = Many
+        self.x = np.random.rand(self.Many)
+        self.r = np.linspace(0, 4.0, num=self.Many)
 
-x = np.random.rand(Many)
-r = np.linspace(0,4.0, num= Many)
+    def plot(self, iterations=100):
+        for i in range(1, iterations + 1):
+            x_a = 1 - self.x
+            Data = np.multiply(self.x, self.r)
+            Data = np.multiply(Data, x_a)
+            self.x = Data
 
-for i in range(1, 54):
-    x_a = 1-x
-    Data= np.multiply(x,r)
-    Data= np.multiply(Data, x_a)
-    x = Data
+            plt.title(r'Logistic map: $x_{n+1} = r x_{n} (1-x_{n}).$  n = ' + str(i))
+            plt.ylabel('x-Random number')
+            plt.xlabel('r-Rate')
 
-    plt.title(r'Logistic map: $x_{n+1} = r x_{n} (1-x_{n}).$  n = '+ str(i) )
-    plt.ylabel('x-Random number')
-    plt.xlabel('r-Rate')
-    
-    plt.scatter(r, Data, s=0.1, c='k')
-    plt.show()
-    plt.savefig(str(i) + " Logistic Map.png", dpi = 300)
-    plt.clf()
-    
+            plt.scatter(self.r, Data, s=0.1, c='k')
+            plt.savefig(str(i) + " Logistic Map.png", dpi=300)
+            plt.clf()
+
+# Example usage:
+if __name__ == "__main__":
+    plotter = LogisticMapPlotter()
+    plotter.plot()
